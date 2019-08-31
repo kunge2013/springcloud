ae8b662f596db43ef4a5a2b6764728810a97eceb
kunge2013 <543976974@QQ.com>
2019-09-01 00:37:15 +0800

    watch

---
+++ springCloudConfig/clientConfig/src/main/java/com/example/demo/ConfigController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+    @Value("${name}")
+    private String name;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return name;
+    }
+}
